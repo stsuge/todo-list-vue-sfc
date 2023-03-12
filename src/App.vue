fb17255da8cafd2e97fed034def0a44f70ca0d34
Shunsuke Tsuge <stsuge1211@gmail.com>
2023-03-12 15:32:38 +0900

    TODOリストの絞り込みラジオボタンを追加

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import StateFilter from './components/StateFilter.vue';
 import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <!-- <header>
+      <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+        <div class="wrapper">
+        <HelloWorld msg="You did it!" />
+        </div>
+        </header> -->
 
-  <main>
-    <TheWelcome />
-  </main>
+  <!-- <main>
+        <TheWelcome />
+      </main> -->
+
+  <StateFilter />
 </template>
 
 <style scoped>
